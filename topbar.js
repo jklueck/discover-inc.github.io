eb8d230d105d05a849654b2647649f721c58cd23
Chedski <35108470+Chedski@users.noreply.github.com>
2019-07-10 12:41:07 -0400

    Made topbar contents change based on window size

--- topbar.js
+++ topbar.js
@@ -1,14 +1,35 @@
-$(function() {
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 50) {
-         $(document.getElementById("navbar")).addClass('scrolled');
-      }
-      if ($(this).scrollTop() < 50) {
-         $(document.getElementById("navbar")).removeClass('scrolled');
-      }
-   });
+// Wait until done loading
+window.onload = function () {
+
+var greetingtext = document.getElementById('greeting');
+var logo = document.getElementById('logo-navbar');
+setGreeting(greetingtext);
+
+// Color change when not scrolled to top of page
+$(window).scroll(function () {
+   if ($(this).scrollTop() > 50) {
+      $(document.getElementById("navbar")).addClass('scrolled');
+   }
+  if ($(this).scrollTop() < 50) {
+     $(document.getElementById("navbar")).removeClass('scrolled');
+   }
 });
 
+// Change what is shown based on screen size
+$(window).resize(function() {
+  if ($(window).width() < 950) {
+    greetingtext.style = "display: none;";
+    
+    if ($(window).width() < 780) {
+      logo.src = "discoverlogosmall.png";
+    } else {
+      logo.src = "discoverlogo.png";
+    }
+  } else {
+  greetingtext.style = "display: unset;";
+  logo.src = "discoverlogo.png";
+ }
+});
 
 // Random greeting messages
 function setGreeting(obj) {
@@ -17,11 +38,9 @@ function setGreeting(obj) {
     "totally not bots",
     "Super big brain",
     "I hate when TechnologicalByte steals my shampoo",
-    "we are so inspired by the millions of incredible <strike>Apple</strike> Roblox developers",
     "petition to add <strike>screebshare</strike> screenshare to servers on discord",
     "you need admin to get admin",
     "It can't be a funny voicechat convo without TechnologicalByte's mic breaking.",
-    "<strike>DontLuck2 di...</strike> <strong>HEY!</strong>",
     "y u do dis",
     "i do logic about 85% of the time",
     "What will Roblox's next logo look like?"];
@@ -31,4 +50,5 @@ function setGreeting(obj) {
   var greeting = greetings[random];
   obj.innerHTML = greeting;
 }
- // Set greeting messages.
+
+};
